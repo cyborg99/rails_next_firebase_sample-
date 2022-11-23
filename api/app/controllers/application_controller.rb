f30a12089a1f76a8b7ee5d5434b0e89b30a7ead9
cyborg99 <m.0.sly.cyborg@gmail.com>
2022-11-24 00:06:49 +0900

    add firebase api

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  private
+
+  def current_user
+    @current_user ||= User.find_and_refresh_token(request.headers[:Authorization])
+  end
 end
