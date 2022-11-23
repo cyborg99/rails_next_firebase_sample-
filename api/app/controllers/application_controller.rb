3b70253d7c6815ef091b2fd4d3049b9470624147
cyborg99 <m.0.sly.cyborg@gmail.com>
2022-11-23 23:43:49 +0900

    stash
    stash
    
    stash
    
    stash
    
    stash
    
    stash
    
    stash

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  private
-
-  def current_user
-    @current_user ||= User.find_and_refresh_token(request.headers[:Authorization])
-  end
 end
--- api/app/graphql/types/mutation_type.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  class MutationType < Types::BaseObject
-    field :sign_up_user, mutation: Mutations::SignUpUser
-  end
-end
